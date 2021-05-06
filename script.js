5fb4a9eb8962095f7a3a0b7591bb917590a856f6
Jesse-Bos <jesse_bos@live.com>
2021-05-06 22:55:31 +0200

    cddd

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//NAVBAR SCROLL JS
 $(document).ready(function () {
   $(window).scroll(function () {
     if (this.scrollY > 20) {
@@ -7,3 +8,18 @@ $(document).ready(function () {
     }
   });
 });
+//---------------------------------------------------------------
+
+// NAVBAR CLOSE ON CLICK
+const toggler = document.querySelector(".navbar-toggler");
+const collapse = document.querySelector(".navbar-collapse");
+
+collapse.addEventListener("click", (e) => {
+  const isCollapsed = document.querySelector(".navbar-collapse.show");
+  const tagName = e.target.tagName;
+
+  if (isCollapsed && (tagName === "A" || tagName === "BUTTON")) {
+    toggler.click();
+  }
+});
+//---------------------------------------------------------------
